179be27e3d71723f2a9f9de6d90747973db85514
JMMR <103889221+robinjmm@users.noreply.github.com>
2023-03-23 08:28:02 +0800

    Fix eslint error when importing fontawesome and styles.scss

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-// eslint-disable-next-line import/no-unresolved
-import "@fontawesome/fontawesome.min.css";
-// eslint-disable-next-line import/no-unresolved
-import "@fontawesome/brands.min.css";
-// eslint-disable-next-line import/no-unresolved
-import "@styles/styles.scss";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
+import "../scss/styles.scss";
 
 const cardOwner = document.querySelector(".js-card-owner");
 const cardNumber = document.querySelector(".js-card-number");
